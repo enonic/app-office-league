24619762bea9ecb2147fa131bfdf509134a74cdd
Alex Rodriguez <aro@enonic.com>
2017-02-02 14:16:20 +0100

    Initialize office-league site

--- src/main/resources/lib/office-league-init.js
+++ src/main/resources/lib/office-league-init.js
@@ -1,11 +1,15 @@
 var repoLib = require('/lib/xp/repo');
 var contextLib = require('/lib/xp/context');
 var nodeLib = require('/lib/xp/node');
+var contentLib = require('/lib/xp/content');
 
 var REPO_NAME = 'office-league';
 var LEAGUES_PATH = '/leagues';
 var PLAYERS_PATH = '/players';
 var TEAMS_PATH = '/teams';
+
+var DEFAULT_SITE_NAME = 'office-league';
+var DEFAULT_SITE_PATH = '/' + DEFAULT_SITE_NAME;
 
 exports.initialize = function () {
     log.info('Initializing Office League repository...');
@@ -32,6 +36,33 @@ var doInitialize = function () {
         createRepo();
     }
     createRootNodes();
+
+    createSite();
+};
+
+var createSite = function () {
+    var result = contentLib.get({
+        key: DEFAULT_SITE_PATH,
+        branch: 'draft'
+    });
+    if (!result) {
+        log.info('Creating Site content...');
+        var siteContent = contentLib.create({
+            name: DEFAULT_SITE_NAME,
+            parentPath: '/',
+            displayName: 'Office League',
+            branch: 'draft',
+            contentType: 'portal:site',
+            data: {
+                // TODO bug in contentLib.create function
+                // siteConfig: {
+                //     applicationKey: app.name,
+                //     config: {}
+                // }
+            },
+            x: {}
+        });
+    }
 };
 
 var createRepo = function () {
@@ -94,7 +125,7 @@ var nodeWithPathExists = function (repoConnection, path) {
     var result = repoConnection.query({
         start: 0,
         count: 0,
-        query: "_path = '"+path+"'"
+        query: "_path = '" + path + "'"
     });
     return result.total > 0;
 };
