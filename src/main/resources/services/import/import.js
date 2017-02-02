95bc64aad8daa005f332f7b26fa9a08b0f356000
Alex Rodriguez <aro@enonic.com>
2017-02-02 16:06:50 +0100

    EXT-329 Office League - Migration. Import teams

--- src/main/resources/services/import/import.js
+++ src/main/resources/services/import/import.js
@@ -12,6 +12,7 @@ var TEAMS_PATH = '/teams';
 exports.get = function (req) {
 
     importPlayers();
+    importTeams();
 
     return {
         contentType: 'application/json',
@@ -38,13 +39,40 @@ var importPlayers = function () {
     }
 };
 
+var importTeams = function () {
+    var teams = fetchTeams();
+    var t;
+    var repoConn = nodeLib.connect({
+        repoId: REPO_NAME,
+        branch: 'master',
+        principals: ["role:system.admin"]
+    });
+
+    log.info(teams.length + ' teams found');
+    for (var i = 0; i < teams.length; i++) {
+        t = teams[i];
+        log.info(JSON.stringify(t));
+        createTeam(repoConn, t);
+    }
+};
+
 var fetchPlayers = function () {
     return contentLib.query({
         start: 0,
         count: -1,
         contentTypes: [LEGACY_APP_NAME + ":player"],
         query: "data.retired != 'true'",
-        branch: 'master'
+        branch: 'draft'
+    }).hits;
+};
+
+var fetchTeams = function () {
+    return contentLib.query({
+        start: 0,
+        count: -1,
+        contentTypes: [LEGACY_APP_NAME + ":team"],
+        query: "data.retired != 'true'",
+        branch: 'draft'
     }).hits;
 };
 
@@ -60,7 +88,7 @@ var createPlayer = function (repoConn, foosPlayer) {
     if (foosImg) {
         pictureContent = contentLib.get({
             key: foosImg,
-            branch: 'master'
+            branch: 'draft'
         });
         if (pictureContent) {
             var attachName = pictureContent.data.media.attachment;
@@ -77,14 +105,71 @@ var createPlayer = function (repoConn, foosPlayer) {
     var playerNode = repoConn.create({
         _name: foosPlayer._name,
         _parentPath: PLAYERS_PATH,
-        name: foosPlayer.data.nickname,
+        name: foosPlayer.displayName,
         nickname: foosPlayer.data.nickname,
         image: imageValue,
         nationality: 'no',
         handedness: 'right',
         description: foosPlayer.data.description
     });
-    log.info(JSON.stringify(playerNode));
+    // log.info(JSON.stringify(playerNode));
+};
+
+var createTeam = function (repoConn, foosTeam) {
+    var teamExist = nodeWithPathExists(repoConn, TEAMS_PATH + '/' + foosTeam._name);
+    if (teamExist) {
+        log.warning('Team with name "' + foosTeam._name + '" already exists, cannot be imported.');
+        return;
+    }
+
+    var foosImg = foosTeam.data.picture;
+    var imageValue = null, pictureContent;
+    if (foosImg) {
+        pictureContent = contentLib.get({
+            key: foosImg,
+            branch: 'draft'
+        });
+        if (pictureContent) {
+            var attachName = pictureContent.data.media.attachment;
+            var stream = contentLib.getAttachmentStream({
+                key: foosImg,
+                name: attachName
+            });
+            if (stream) {
+                imageValue = valueLib.binary(attachName, stream);
+            }
+        }
+    }
+    var players = [];
+    players.push(findPlayerNodeById(repoConn, foosTeam.data.playerIds[0]));
+    players.push(findPlayerNodeById(repoConn, foosTeam.data.playerIds[1]));
+
+    var teamNode = repoConn.create({
+        _name: foosTeam._name,
+        _parentPath: TEAMS_PATH,
+        name: foosTeam.displayName,
+        image: imageValue,
+        description: foosTeam.data.description,
+        playerIds: players
+    });
+    log.info(JSON.stringify(teamNode));
+};
+
+var findPlayerNodeById = function (repoConn, playerContentId) {
+    var p = contentLib.get({
+        key: playerContentId,
+        branch: 'draft'
+    });
+    if (!p) {
+        return null;
+    }
+    log.info(JSON.stringify(p));
+
+    var path = PLAYERS_PATH + '/' + p._name;
+    log.info(path);
+    var result = repoConn.get(path);
+    log.info(JSON.stringify(result));
+    return result && result._id;
 };
 
 var nodeWithPathExists = function (repoConnection, path) {
