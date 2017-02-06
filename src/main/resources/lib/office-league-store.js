fefb273dbc9fb56992d4f6da0b41d51d68ff5811
Alex Rodriguez <aro@enonic.com>
2017-02-06 12:24:01 +0100

    EXT-333 Office League - Storage lib. Retrieve players.

--- src/main/resources/lib/office-league-store.js
+++ src/main/resources/lib/office-league-store.js
@@ -31,6 +31,24 @@ var TYPE = {
  */
 
 /**
+ * @typedef {Object} Player
+ * @property {string} type Object type: 'player'
+ * @property {string} name Name of the player.
+ * @property {string} nickname Nickname of the player.
+ * @property {string} image Binary name of the player's image.
+ * @property {string} nationality 2 letter country code of the player (https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).
+ * @property {string} handedness Player handedness: 'right', 'left', 'ambidexterity.
+ * @property {string} description Description text.
+ */
+
+/**
+ * @typedef {Object} PlayerResponse
+ * @property {Player[]} players Array of player objects.
+ * @property {number} count Total number of players.
+ * @property {number} total Count of players returned.
+ */
+
+/**
  * Retrieve a list of leagues.
  * @param  {number} [start=0] First index of the leagues.
  * @param  {number} [count=10] Number of leagues to fetch.
@@ -70,7 +88,8 @@ exports.getLeagues = function (start, count) {
 exports.getLeagueById = function (leagueId) {
     var repoConn = newConnection();
 
-    return repoConn.get(leagueId);
+    var obj = repoConn.get(leagueId);
+    return obj && (obj.type === TYPE.LEAGUE) ? obj : null;
 };
 
 /**
@@ -96,12 +115,71 @@ exports.getLeagueByName = function (name) {
     return league;
 };
 
-exports.getPlayers = function () {
+/**
+ * Retrieve a list of players.
+ * @param  {number} [start=0] First index of the players.
+ * @param  {number} [count=10] Number of players to fetch.
+ * @return {PlayerResponse} Players.
+ */
+exports.getPlayers = function (start, count) {
+    var repoConn = newConnection();
 
+    start = start || 0;
+    count = count || 10;
+    var result = repoConn.query({
+        start: start,
+        count: count,
+        query: "type = '" + TYPE.PLAYER + "'"
+    });
+
+    var players = [];
+    if (result.count > 0) {
+        var ids = result.hits.map(function (hit) {
+            return hit.id;
+        });
+        players = [].concat(repoConn.get(ids));
+    }
+
+    return {
+        "total": result.total,
+        "count": result.count,
+        "players": players
+    };
 };
 
+/**
+ * Retrieve a player by its id.
+ * @param  {string} playerId Id of the player.
+ * @return {Player} Player object or null if not found.
+ */
 exports.getPlayerById = function (playerId) {
+    var repoConn = newConnection();
 
+    var obj = repoConn.get(playerId);
+    return obj && (obj.type === TYPE.PLAYER) ? obj : null;
+};
+
+/**
+ * Retrieve a player by its name.
+ * @param  {string} name Name of the player.
+ * @return {Player} Player object or null if not found.
+ */
+exports.getPlayerByName = function (name) {
+    var repoConn = newConnection();
+
+    var result = repoConn.query({
+        start: 0,
+        count: 1,
+        query: "type = '" + TYPE.PLAYER + "' AND name='" + name + "'"
+    });
+
+    var player;
+    if (result.count > 0) {
+        var id = result.hits[0].id;
+        player = repoConn.get(id);
+    }
+
+    return player;
 };
 
 var newConnection = function () {
--- src/main/resources/services/test/test.js
+++ src/main/resources/services/test/test.js
@@ -7,7 +7,10 @@ exports.get = function (req) {
         body: {
             leagues: storeLib.getLeagues(),
             leagueById: storeLib.getLeagueById('db0af7db-6a11-4242-b124-63467e58449c'),
-            leagueByName: storeLib.getLeagueByName('Enonic Foos')
+            leagueByName: storeLib.getLeagueByName('Enonic Foos'),
+            players: storeLib.getPlayers(0, 10),
+            playerById: storeLib.getPlayerById('341a3117-fda7-4ef3-8827-592b819121f7'),
+            playerByName: storeLib.getPlayerByName('aro')
         }
     }
 };
