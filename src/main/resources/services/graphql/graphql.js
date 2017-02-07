a9ec91e0e23938c1782742056ed4fc14941d8a00
Glenn Ricaud <gri@enonic.com>
2017-02-07 14:21:03 +0100

    EXT-340 Office League - Graph QL - Game queries

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -63,9 +63,95 @@ var teamType = graphQlLib.createType('Team', {
         players: {
             type: graphQlLib.list(playerType),
             data: function (env) {
-                return env.source.playerIds.map(function (playerId) {
+                return toArray(env.source.playerIds).map(function (playerId) {
                     return storeLib.getPlayerById(playerId);
                 });
+            }
+        }
+    }
+);
+
+var pointType = graphQlLib.createType('Point', {
+        id: {
+            type: graphQlLib.scalar('ID'),
+            data: function (env) {
+                return env.source._id;
+            }
+        },
+        time: {
+            type: graphQlLib.scalar('Int'),
+            data: function (env) {
+                return env.source.time;
+            }
+        },
+        against: {
+            type: graphQlLib.scalar('Boolean'),
+            data: function (env) {
+                return env.source.against;
+            }
+        },
+        player: {
+            type: playerType,
+            data: function (env) {
+                return storeLib.getPlayerById(env.source.playerId);
+            }
+        }
+    }
+);
+
+var commentType = graphQlLib.createType('Comment', {
+        id: {
+            type: graphQlLib.scalar('ID'),
+            data: function (env) {
+                return env.source._id;
+            }
+        },
+        text: {
+            type: graphQlLib.scalar('String'),
+            data: function (env) {
+                return env.source.text;
+            }
+        },
+        author: {
+            type: playerType,
+            data: function (env) {
+                return storeLib.getPlayerById(env.source.authorId);
+            }
+        },
+        likes: {
+            type: graphQlLib.list(playerType),
+            data: function (env) {
+                return toArray(env.source.likes).map(function (playerId) {
+                    storeLib.getPlayerById(playerId)
+                });
+            }
+        }
+    }
+);
+
+var gameType = graphQlLib.createType('Game', {
+        id: {
+            type: graphQlLib.scalar('ID'),
+            data: function (env) {
+                return env.source._id;
+            }
+        },
+        time: {
+            type: graphQlLib.scalar('String'),
+            data: function (env) {
+                return env.source.time;
+            }
+        },
+        finished: {
+            type: graphQlLib.scalar('Boolean'),
+            data: function (env) {
+                return env.source.finished;
+            }
+        },
+        points: {
+            type: graphQlLib.list(pointType),
+            data: function (env) {
+                return env.source.points;
             }
         }
     }
@@ -122,11 +208,33 @@ var schema = graphQlLib.createSchema({
                 var count = env.args.count;
                 return storeLib.getTeams(start, count).teams;
             }
+        },
+        games: {
+            type: graphQlLib.list(gameType),
+            args: {
+                leagueId: graphQlLib.scalar('ID'),
+                start: graphQlLib.scalar('Int'),
+                count: graphQlLib.scalar('Int')
+            },
+            data: function (env) {
+                var leagueId = env.args.leagueId;
+                var start = env.args.start;
+                var count = env.args.count;
+                return storeLib.getLeagueGames(leagueId, start, count).games;
+            }
         }
-
     }
 });
 
+function toArray(object, callback) {
+    if (!object) {
+        return [];
+    }
+    if (object.constructor === Array) {
+        return object;
+    }
+    return [object];
+}
 
 exports.post = function (req) {
     var body = JSON.parse(req.body);
