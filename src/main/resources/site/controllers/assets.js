a424f0e51759fc531cb74a16f516cc90f21940e3
Alex Rodriguez <aro@enonic.com>
2017-03-15 14:41:40 +0100

    Fixed error in team-profile page

--- src/main/resources/site/controllers/assets.js
+++ src/main/resources/site/controllers/assets.js
@@ -4,8 +4,6 @@ exports.get = function (req) {
     var path = req.url.split('app/assets/')[1];
     var type = ioLib.getMimeType(path);
 
-    log.info('Serving asset of type: ' + type + ' at ' + path);
-
     return {
         contentType: type,
         body: ioLib.getResource('/assets/' + path).getStream()
