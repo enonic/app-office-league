ce8237758d3f785ff5735c5ec8c9aabd303554b8
Alex Rodriguez <aro@enonic.com>
2017-03-15 13:30:15 +0100

    Detect page navigation changes to toggle main toolbar

--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Location} from '@angular/common';
 import {AuthService} from './auth.service';
 import {ImageService} from './image.service';
+import {Router, NavigationStart} from '@angular/router';
 
 @Component({
     selector: 'office-league',
@@ -12,10 +13,16 @@ export class AppComponent {
     private logoUrl: string;
     private isPlayingGame: boolean;
 
-    constructor(private auth: AuthService, private location: Location) {
+    constructor(private auth: AuthService, private location: Location, private router: Router) {
         this.logoUrl = ImageService.logoUrl();
 
         this.isPlayingGame = location.path().startsWith('/games/');
         console.log(this.isPlayingGame);
+
+        router.events
+            .filter(event => event instanceof NavigationStart)
+            .subscribe((event: NavigationStart) => {
+                this.isPlayingGame = event.url.startsWith('/games/');
+            });
     }
 }
