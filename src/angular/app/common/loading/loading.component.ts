32663cc56283ef6e3d532340f3d24ce0189f53d5
Glenn Ricaud <gri@enonic.com>
2017-03-29 10:53:01 +0200

    Office League - Loader (#137)

--- src/angular/app/common/loading/loading.component.ts
+++ src/angular/app/common/loading/loading.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'loading',
-    templateUrl: 'loading.component.html'
+    templateUrl: 'loading.component.html',
+    styleUrls: ['loading.component.less']
 })
 export class LoadingComponent {
 
