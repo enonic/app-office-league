77f88ac98cee64cf550c3d2d1bffcf035252f016
Alex Rodriguez <aro@enonic.com>
2017-04-06 11:31:22 +0200

    User-player link service

--- src/main/resources/services/player-link/player-link.js
+++ src/main/resources/services/player-link/player-link.js
@@ -4,6 +4,7 @@ var storeLib = require('/lib/office-league-store');
 // Assign currently logged in user with a player. For testing purposes.
 exports.get = function (req) {
     var playerName = req.params['player'];
+    var userKey = req.params['user'];
     var unlink = req.params['unlink'] === 'true';
     if (!playerName) {
         return {
@@ -36,6 +37,40 @@ exports.get = function (req) {
             }
         }
     }
+    if (userKey) {
+        if (!authLib.hasRole('system.admin')) {
+            return {
+                contentType: 'application/json',
+                status: 403,
+                body: {
+                    message: 'Not enough rights'
+                }
+            }
+        }
+        var principal = authLib.getPrincipal('user:' + userKey);
+        if (!principal) {
+            return {
+                contentType: 'application/json',
+                status: 400,
+                body: {
+                    message: 'User not found: ' + '[user:' + userKey + ']'
+                }
+            }
+        }
+        storeLib.updatePlayer({
+            playerId: player._id,
+            userKey: principal.key
+        });
+        storeLib.refresh();
+        return {
+            contentType: 'application/json',
+            status: 200,
+            body: {
+                message: 'Player [' + player.name + '] assigned to user [' + principal.displayName + ']'
+            }
+        }
+    }
+
     if (unlink) {
         storeLib.updatePlayer({
             playerId: player._id,
