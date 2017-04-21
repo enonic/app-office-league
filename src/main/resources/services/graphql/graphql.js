d81abff2eead10a0faaa50beae266de25e479821
Glenn Ricaud <gri@enonic.com>
2017-04-21 10:58:31 +0200

    Hackaton

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -1,9 +1,15 @@
+var contextLib = require('/lib/xp/context');
 var graphQlLib = require('graphql');
 var schemaLib = require('./graphql-schema');
 
 exports.post = function (req) {
     var body = JSON.parse(req.body);
-    var result = graphQlLib.execute(schemaLib.schema, body.query, body.variables);
+
+    var result = contextLib.run({
+        principals: ["role:system.admin", "role:system.authenticated"]
+    }, function () {
+        return graphQlLib.execute(schemaLib.schema, body.query, body.variables);
+    });
     return {
         contentType: 'application/json',
         body: result
