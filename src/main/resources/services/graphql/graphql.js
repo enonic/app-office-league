fe4d77f3f66a8c52eebc549547dcc29fba9fc456
Glenn Ricaud <gri@enonic.com>
2017-04-24 14:49:10 +0200

    Reverse hackaton modification

--- src/main/resources/services/graphql/graphql.js
+++ src/main/resources/services/graphql/graphql.js
@@ -1,15 +1,9 @@
-var contextLib = require('/lib/xp/context');
 var graphQlLib = require('graphql');
 var schemaLib = require('./graphql-schema');
 
 exports.post = function (req) {
     var body = JSON.parse(req.body);
-
-    var result = contextLib.run({
-        principals: ["role:system.admin", "role:system.authenticated"]
-    }, function () {
-        return graphQlLib.execute(schemaLib.schema, body.query, body.variables);
-    });
+    var result = graphQlLib.execute(schemaLib.schema, body.query, body.variables);
     return {
         contentType: 'application/json',
         body: result
