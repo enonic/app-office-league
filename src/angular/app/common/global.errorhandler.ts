0245f3149edfa43b0f8db075c13bd2f5b3fa206e
Alex Rodriguez <aro@enonic.com>
2017-04-26 15:38:15 +0200

    Friendlier error message

--- src/angular/app/common/global.errorhandler.ts
+++ src/angular/app/common/global.errorhandler.ts
@@ -6,7 +6,7 @@ export class GlobalErrorHandler implements ErrorHandler {
     handleError(error) {
         console.error('Global error handler showing toast for: ' + error.toString());
 
-        Materialize.toast(`<i class="material-icons">error_outline</i><p>${error.toString()}</p>`, 10000, 'error');
+        Materialize.toast(`<i class="material-icons">error_outline</i><p>Oops! Something went wrong!</p>`, 3000, 'error');
 
         // IMPORTANT: Rethrow the error otherwise it gets swallowed
         //throw error;
