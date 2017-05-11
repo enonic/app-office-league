9e1785a8232d0014e0de2060634ca7b8add82a29
Alex Rodriguez <aro@enonic.com>
2017-05-11 13:05:57 +0200

    Office League - Minor bugs - Batch 2 (#212) See more teams: Remove player name/picture

--- src/angular/app/players/player-team-list/player-team-list.component.ts
+++ src/angular/app/players/player-team-list/player-team-list.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {BaseComponent} from '../../common/base.component';
-import {Player} from '../../../graphql/schemas/Player';
 import {Team} from '../../../graphql/schemas/Team';
 import {GraphQLService} from '../../services/graphql.service';
 import {PageTitleService} from '../../services/page-title.service';
@@ -15,8 +14,6 @@ export class PlayerTeamListComponent extends BaseComponent {
     private static readonly paging = 10;
     private static readonly getPlayerQuery = `query($name: String, $after:Int,$first:Int) {
         player(name:$name) {
-            name
-            imageUrl
             teamsConnection(after:$after, first:$first) {
                 totalCount
                 edges {
@@ -37,7 +34,6 @@ export class PlayerTeamListComponent extends BaseComponent {
     }`;
 
     private playerName: string;
-    public player: Player;
     private teams: Team[];
     private pageCount: number = 1;
 
@@ -58,7 +54,6 @@ export class PlayerTeamListComponent extends BaseComponent {
             PlayerTeamListComponent.getPlayerQuery,
             {name: this.playerName, after: after, first:PlayerTeamListComponent.paging },
             data => {
-                this.player = Player.fromJson(data.player);
                 this.teams = data.player.teamsConnection.edges.map(edge => Team.fromJson(edge.node));
                 let totalCount = data.player.teamsConnection.totalCount;
                 this.pageCount = Math.floor((totalCount == 0 ? 0: totalCount - 1) / PlayerTeamListComponent.paging) + 1;
