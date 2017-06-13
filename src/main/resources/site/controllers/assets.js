9111d283b10b741e1936046c69a84e6fb3664d80
Alex Rodriguez <aro@enonic.com>
2017-06-13 11:41:27 +0200

    Office League - Mp3 audio not playing in Firefox (#292)

--- src/main/resources/site/controllers/assets.js
+++ src/main/resources/site/controllers/assets.js
@@ -2,10 +2,27 @@ var ioLib = require('/lib/xp/io');
 
 exports.get = function (req) {
     var path = req.url.split('app/assets/')[1];
-    var type = ioLib.getMimeType(path);
+    var type = getMimeType(path);
 
     return {
         contentType: type,
         body: ioLib.getResource('/assets/' + path).getStream()
     };
+};
+
+var getMimeType = function (path) {
+    var type = ioLib.getMimeType(path);
+    if (type && type !== 'application/octet-stream') {
+        return type
+    }
+
+    var ext = path.indexOf('.') > 0 ? path.split('.').pop().toLowerCase() : '';
+
+    if (ext.indexOf('mp3') > -1) {
+        type = 'audio/mp3';
+    } else if (ext.indexOf('wav') > -1) {
+        type = 'audio/wav';
+    }
+
+    return type;
 };
