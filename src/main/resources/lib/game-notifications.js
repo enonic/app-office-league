1c06faf330a7337057c70c2abdbd9f37a6edbff3
Alex Rodriguez <aro@enonic.com>
2017-11-01 07:58:51 +0100

    Send notifications only when the game is started

--- src/main/resources/lib/game-notifications.js
+++ src/main/resources/lib/game-notifications.js
@@ -1,5 +1,6 @@
 var eventLib = require('/lib/xp/event');
 var storeLib = require('office-league-store');
+var authLib = require('/lib/xp/auth');
 
 var OFFICE_LEAGUE_GAME_EVENT_ID = storeLib.OFFICE_LEAGUE_GAME_EVENT_ID;
 
@@ -35,16 +36,17 @@ var sendGameNotifications = function (gameId, leagueId) {
     var url = baseUrl + '/games/' + game._id;
 
     var points = [].concat(game.points || []);
-    if (game.finished) {
-        log.info('Sending push notifications for ended game: ' + gameId);
-
-        storeLib.sendPushNotification({
-            playerIds: playerIds,
-            text: "A game has ended in '" + league.name + "'",
-            url: url
-        });
-
-    } else if (points.length === 0) {
+    // if (game.finished) {
+    //     log.info('Sending push notifications for ended game: ' + gameId);
+    //
+    //     storeLib.sendPushNotification({
+    //         playerIds: playerIds,
+    //         text: "A game has ended in '" + league.name + "'",
+    //         url: url
+    //     });
+    //
+    // } else
+    if (points.length === 0) {
         log.info('Sending push notifications for new game: ' + gameId);
 
         storeLib.sendPushNotification({
@@ -54,4 +56,25 @@ var sendGameNotifications = function (gameId, leagueId) {
         });
 
     }
+};
+
+var excludeCurrentPlayer = function (playerIds) {
+    var currentPlayerId = getCurrentPlayerId();
+    log.info('currentPlayerId: ' + currentPlayerId);
+    if (!currentPlayerId) {
+        return playerIds;
+    }
+    return playerIds.filter(function (pid) {
+        return pid !== currentPlayerId;
+    });
+};
+
+var getCurrentPlayerId = function () {
+    var user = authLib.getUser();
+    log.info('user: ' + JSON.stringify(user, null, 2));
+    if (!user) {
+        return null;
+    }
+    var player = storeLib.getPlayerByUserKey(user.key);
+    return player && player._id;
 };
