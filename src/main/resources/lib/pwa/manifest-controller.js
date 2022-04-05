26036b08e5dd58cb78758c5143b45af8ffc8fd05
Persijn Kwekkeboom <pkw@enonic.com>
2022-04-05 11:05:23 +0200

    Manifest.json #675
    Added relative start_url

--- src/main/resources/lib/pwa/manifest-controller.js
+++ src/main/resources/lib/pwa/manifest-controller.js
@@ -3,10 +3,7 @@ var mustache = require('/lib/mustache');
 
 exports.get = function(req) {
 
-    var bean = __.newBean('com.enonic.app.officeleague.VhostHandler');
-    const vhost = bean.getVirtualHost();
-
-    var startUrl = vhost + "/app";
+    const startUrl = "./";
 
     var params = {
         startUrl,
