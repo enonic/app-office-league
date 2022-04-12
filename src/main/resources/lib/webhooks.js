7429c6bc1b31bd9e6a61bc1f3b4a9581ebe7660f
Persijn Kwekkeboom <pkw@enonic.com>
2022-04-12 18:58:33 +0200

    New slack bot integration #691
    New message format for slack bot integration
    Changed the data displayed in message

--- src/main/resources/lib/webhooks.js
+++ src/main/resources/lib/webhooks.js
@@ -6,6 +6,9 @@ var httpClient = require('/lib/http-client');
 var OFFICE_LEAGUE_GAME_REPORT_EVENT_ID = reportLib.OFFICE_LEAGUE_GAME_REPORT_EVENT_ID;
 var UUID = Java.type('java.util.UUID');
 
+/**
+ * Setup an event listner so the webhook can handle the data
+ */
 exports.setupWebHooks = function () {
     eventLib.listener({
         type: 'custom.' + OFFICE_LEAGUE_GAME_REPORT_EVENT_ID,
@@ -22,7 +25,11 @@ exports.setupWebHooks = function () {
     });
 };
 
-var processGameWebhooks = function (game) {
+/***
+ * Goes over all webhooks and tried processes the games
+ * @param game -- Json data of the current game event
+*/
+const processGameWebhooks = function (game) {
     var urls = [];
     for (var key in app.config) {
         if (key.indexOf('webhooks.server.') === 0 && key.endsWith('.url')) {
@@ -31,19 +38,24 @@ var processGameWebhooks = function (game) {
     }
 
     for (var u = 0; u < urls.length; u++) {
-        try {
+        /* try { */
             processGameWebhook(game, urls[u]);
-        } catch (e) {
+       /*  } catch (e) {
             log.warning('Error posting to webhook: ' + urls[u]);
             if (e.printStackTrace) {
                 e.printStackTrace();
             }
-        }
+        } */
     }
 };
 
-var processGameWebhook = function (gameJson, url) {
-    var event = {
+/***
+ * Creates the message data and send it to the the url
+ * @param gameJson json data of the game
+ * @param url url to the webhook where the game data will be sent
+ */
+const processGameWebhook = function (gameJson, url) {
+    const event = {
         id: "evt_" + randomId(),
         event: "game.updated",
         created: Math.floor(new Date().getTime() / 1000),
@@ -52,17 +64,18 @@ var processGameWebhook = function (gameJson, url) {
         }
     };
 
-    var response = httpClient.request({
-        url: url,
+    // log.info(JSON.stringify(gameJson, null, 4));
+
+    const response = httpClient.request({
+        url,
         method: 'POST',
         contentType: 'application/json',
-        body: JSON.stringify(event),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+        body: JSON.stringify(gameJson),
     });
 
     if (response.status >= 300 || response.status < 200) {
+        // log.info(JSON.stringify(gameJson, null, 4));
+        // log.info(JSON.stringify(response, null, 4));
         log.info('Error posting webhook to [' + url + ']: ' + response.status + ' ' + response.message);
     }
 };
